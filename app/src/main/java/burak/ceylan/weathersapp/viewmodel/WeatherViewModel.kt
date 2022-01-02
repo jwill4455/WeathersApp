0f50117f9ed903281006fd3813a734052212ebbd
Burak <burakceylann@outlook.com>
2022-01-02 15:53:59 +0300

    Add logic get weather by city's name

---
+++ app/src/main/java/burak/ceylan/weathersapp/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,63 @@
+package burak.ceylan.weathersapp.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import burak.ceylan.weathersapp.model.citysearch.CitySearch
+import burak.ceylan.weathersapp.model.dailyforecast.DailyForecast
+import burak.ceylan.weathersapp.model.mygeolocation.MyGeoLocation
+import burak.ceylan.weathersapp.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(private val weatherRepository: WeatherRepository): ViewModel() {
+    private val viewModelJob = Job()
+    private val coroutineContext = CoroutineScope(Dispatchers.Main + viewModelJob)
+    val weatherLiveData = MutableLiveData<DailyForecast>()
+
+
+    fun getWeatherHere(latitude: Double, longitude: Double){
+        try {
+            coroutineContext.launch {
+                val response = weatherRepository.getKeyForCurrentLocation(latitude, longitude)
+                response?.let {
+                    val key = it.key
+                    val weather = weatherRepository.getWeatherByKey(key = key)
+                    weather?.let { weather ->
+                        weatherLiveData.value = weather
+                    }
+                }
+                Log.e("TAG", "response $response")
+            }
+
+        }catch (ex:Exception) {
+        }
+
+    }
+
+    fun getWeatherOfCityByName(cityName: String){
+        try {
+            coroutineContext.launch {
+                val response = weatherRepository.getLocationOfCityByName(cityName)
+                response?.let {
+                    it.firstOrNull()?.let { city ->
+                        val key = city.key
+                        val weather = weatherRepository.getWeatherByKey(key = key)
+                        weather?.let { weather ->
+                            weatherLiveData.value = weather
+                        }
+                    }
+                }
+                Log.e("TAG", "response $response")
+            }
+
+        }catch (ex:Exception) {
+        }
+
+    }
+}
