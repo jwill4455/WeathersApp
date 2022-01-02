8dfee0992be881a299067a78fec63242d84f50d4
Burak <burakceylann@outlook.com>
2022-01-02 21:17:19 +0300

    Add adapter for rcv city

--- app/src/main/java/burak/ceylan/weathersapp/view/home/HomeFragment.kt
+++ app/src/main/java/burak/ceylan/weathersapp/view/home/HomeFragment.kt
@@ -1,10 +1,8 @@
-package burak.ceylan.weathersapp.view
+package burak.ceylan.weathersapp.view.home
 
 import android.Manifest
-import android.content.Context
 import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
-import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
@@ -13,9 +11,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import burak.ceylan.weathersapp.R
+import burak.ceylan.weathersapp.adapter.AdapterCity
+import burak.ceylan.weathersapp.database.entity.CityEntity
+import burak.ceylan.weathersapp.view.MainActivity
 import burak.ceylan.weathersapp.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -24,9 +27,13 @@ import pub.devrel.easypermissions.EasyPermissions
 
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AdapterCity.CityListener {
     private val weatherViewModel :WeatherViewModel by activityViewModels()
     private var  mLocationManager: LocationManager? = null
+    private var cityList = mutableListOf<CityEntity>()
+    private var cityName = ""
+    private var adapterCity: AdapterCity? = null
+    private var isGetWeatherHere = true
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +42,11 @@ class HomeFragment : Fragment() {
     }
 
     private val mLocationListener: LocationListener =
-        LocationListener { p0 -> Log.e("tag", "location ${p0.latitude} ${p0.longitude}") }
+        LocationListener { p0 ->
+            val latitude = p0.latitude
+            val longitude = p0.longitude
+            weatherViewModel.getWeatherHere(latitude, longitude)
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,23 +58,39 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRcv()
         activity?.let { it ->
             mLocationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
-
-
+        }
+        requestLocationPermission()
+        button?.setOnClickListener {
+            weatherViewModel.insertCity(CityEntity(id = 1, name = "Test", key = "asdfasdf"))
         }
 
-        button?.setOnClickListener {
-            //test
-           // weatherViewModel.getWeatherHere(41.015137, 28.979530)\
-            requestLocationPermission()
+        editText?.addTextChangedListener {
+           weatherViewModel.getAllCity()
+
+        }
+        weatherViewModel.weatherLiveData.observe(viewLifecycleOwner, Observer{
+
+        })
+
+        weatherViewModel.cityLiveData.observe(viewLifecycleOwner, { list ->
+            Log.e("tag", "list ${list?.toString()} ${list?.size}")
+        })
+    }
+
+    private fun setupRcv() {
+        adapterCity = AdapterCity(this)
+        rcvCity?.apply {
+            adapter = adapterCity
         }
     }
 
     @AfterPermissionGranted(123)
     private fun requestLocationPermission() {
         val perms =
-            arrayOf<String>(
+            arrayOf(
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION
             )
@@ -97,6 +124,11 @@ class HomeFragment : Fragment() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onCityClicked(city: CityEntity) {
+        rcvCity?.visibility = View.GONE
+        weatherViewModel.getWeatherCityByKey(key = city.key)
     }
 
 }
