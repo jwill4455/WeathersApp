4256cf90e77d357bcb7f422353dc48681215eb11
Burak <burakceylann@outlook.com>
2022-01-02 16:09:54 +0300

    Add api search city and get weather

---
+++ app/src/main/java/burak/ceylan/weathersapp/service/WeatherApi.kt
@@ -0,0 +1,27 @@
+package burak.ceylan.weathersapp.service
+
+import burak.ceylan.weathersapp.model.citysearch.CitySearch
+import burak.ceylan.weathersapp.model.dailyforecast.DailyForecast
+import burak.ceylan.weathersapp.model.mygeolocation.MyGeoLocation
+import com.google.gson.JsonElement
+import retrofit2.Response
+import retrofit2.http.*
+
+interface WeatherApi {
+
+    companion object {
+        const val BASE_URL = "https://dataservice.accuweather.com/"
+        const val API_KEY = "qfuYpQiWEEajMkBYZRHu137gbcmctiOT"
+    }
+
+    @GET("locations/v1/cities/geoposition/search")
+    suspend fun getKeyForCurrentLocation(@QueryMap param: HashMap<String, String>): Response<MyGeoLocation>
+
+    @GET("locations/v1/cities/search")
+    suspend fun getLocationOfCityByName(@QueryMap param: HashMap<String, String>): Response<CitySearch>
+
+    @GET("forecasts/v1/daily/1day/{key}")
+    suspend fun getWeatherByKey(@Path("key") id: String): Response<DailyForecast>
+
+
+}
