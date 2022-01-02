6a3f44bc10ef661c04488f2386fbbd17b93b2fad
Burak <burakceylann@outlook.com>
2022-01-02 21:43:33 +0300

    Add logic for display Forecast

--- app/src/main/java/burak/ceylan/weathersapp/view/DetailFragment.kt
+++ app/src/main/java/burak/ceylan/weathersapp/view/DetailFragment.kt
@@ -6,8 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import burak.ceylan.weathersapp.R
+import burak.ceylan.weathersapp.adapter.AdapterWeather
+import burak.ceylan.weathersapp.model.dailyforecast.Temperature
+import burak.ceylan.weathersapp.view.home.HomeFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +27,16 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val list = arguments?.getParcelableArrayList<Temperature>(HomeFragment.KEY)
+        list?.let { list ->
+            rcvWeather?.apply {
+                adapter = AdapterWeather(list = list)
+            }
+        }
     }
 
 
