c01ce426f4030941d5c1f88611ef5e02f7514b4c
Burak <burakceylann@outlook.com>
2022-01-03 12:57:44 +0300

    Add back arrow for navigation

--- app/src/main/java/burak/ceylan/weathersapp/view/MainActivity.kt
+++ app/src/main/java/burak/ceylan/weathersapp/view/MainActivity.kt
@@ -3,6 +3,9 @@ package burak.ceylan.weathersapp.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import burak.ceylan.weathersapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,9 +13,22 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // Here I created NavigationBackToolbar arrow top left of the screen "<-"
+    private lateinit var navigationController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //BACK ARROW ON TOOLBAR
+        navigationController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navigationController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationController, null)
     }
 
 }
