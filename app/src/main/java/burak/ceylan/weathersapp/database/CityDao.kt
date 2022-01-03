0a06672d38a26d488e64978f273a8fc68a6af98a
Burak <burakceylann@outlook.com>
2022-01-03 12:33:14 +0300

    Refactor code

--- app/src/main/java/burak/ceylan/weathersapp/database/CityDao.kt
+++ app/src/main/java/burak/ceylan/weathersapp/database/CityDao.kt
@@ -1,21 +1,20 @@
 package burak.ceylan.weathersapp.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import burak.ceylan.weathersapp.database.entity.CityEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CityDao {
     @Insert
-    suspend fun insertCity(favoriteUser: CityDao)
+    suspend fun insertCity(city: CityEntity)
 
     @Query("SELECT * FROM City")
     fun getAllCity(): LiveData<List<CityEntity>>
 
     @Query("DELETE FROM City WHERE City.id = :id")
     suspend fun removeCity(id: Int): Int
+
+
+
 }
